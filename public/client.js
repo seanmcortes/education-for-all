8ffc011cef1779be2fdaa25a7bd8b34d4cb42656
Sean Cortes <cortess@oregonstate.edu>
2018-11-11 14:28:35 -0800

    create views

---
+++ public/client.js
@@ -0,0 +1,10 @@
+// Program: Education For All
+// Authors: Jason Anderson, Sean Cortes, Joel Huffman, Tingting Lin, Ting Ju Sheppy
+// Concept: Jacob Carter
+
+
+// Place helper functions such as button clicks and ajax calls here:
+
+
+
+
