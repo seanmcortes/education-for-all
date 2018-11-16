3315290856adcc1fa4098e21514afc9c59f20665
Joel Huffman <huffmajo@oregonstate.edu>
2018-11-15 21:40:02 -0800

    login.hbs update

--- public/client.js
+++ public/client.js
@@ -4,18 +4,3 @@
 
 
 // Place helper functions such as button clicks and ajax calls here:
-
-document.addEventListener("DOMContentLoaded", bindButtons);
-
-function bindButtons()
-{
-	document.getElementById("login").addEventListener("click", function(event)
-	{
-		var usernameField = document.getElementById("username").value
-		var passwordField = document.getElementById("password").value
-
-		//code for checking username and password against db values 
-	})
-}
-
-
