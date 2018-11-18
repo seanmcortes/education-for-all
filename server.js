7b08c8bd97dd4c7a2adc95dfe8bdd51413b9e02b
Sean Cortes <cortess@oregonstate.edu>
2018-11-18 13:56:31 -0800

    prep merge with master

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.use(express.static('public'));
 app.use(session({
   secret: 'group18',
   authenticated: false,
-  // user_id: 0,
-  // user_type: '',
   resave: true,
   saveUninitialized: true
 }));
@@ -131,16 +129,229 @@ app.post('/admin/createuser', checkAdmin, function(req, res, next) {
 })
 
 
-// Render dashboard page for students
-app.get('/dashboard', checkAuth, function(req, res, next) {
-  if (req.session.user_type == "admin"){
-    res.redirect('/admin/createuser');
-  } else {
-    var context = {};
-    context.test = "Hello World!";
-    res.render('dashboard', context);
-  }
+// Display profile page
+app.get('/profile', function(req, res, next) {
+  var context = {};
+  mysql.pool.query(
+    'SELECT ' + req.session.user_type + '_id, first_name, last_name, DOB, identification, user_id FROM ' + req.session.user_type + ' WHERE user_id = ' + req.session.user_id,
+    function (err, results, fields) {
+      if(error){
+        res.write(JSON.stringify(error));
+        res.end();
+      } else {
+        context.identification = results[0].identification;
+        res.render('profile', context);
+      }
+  });
 });
+
+/*********** home page **********/
+app.get('/', function (req, res) {
+    var context = {};
+   context.text = "This is home page!";
+    res.render('home', context);
+});
+
+/*********** dashboard page including the link of selected courses **********/
+
+//return html page for dashboard
+
+//example : enter http://localhost:3000/dashboard to get the course overview for student id 1.
+app.get('/dashboard', function (req, res) {
+
+  //debug, the function works.
+  // req.session.user_id = 1; 
+
+  var context = {};
+  getDashboardByStudentId(req.session.user_id, 
+    function(courseLink) {
+      context.courseLink = courseLink;
+      res.render('dashboard', context);
+    },
+    function(error) {
+      context.errorMessage = JSON.stringify(error);
+      res.render('500', context);
+    }
+  );
+});
+
+//get dashboard API
+app.get('/getDashboard', function(req, res, next){
+
+  //debug 
+  // req.session.user_id = 1; 
+
+
+  getDashboardByStudentId(req.session.user_id,
+  function(courseLink) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(courseLink));
+  },
+  function(error) {
+    res.send(JSON.stringify(error));
+  }
+);
+});
+
+var getDashboardByStudentId = function(studentId, success, failure) {
+  if (!studentId) {
+    var errorMessage = "studentId is invalid";
+    console.log(errorMessage);
+    failure(errorMessage);
+    return;
+  }
+
+  mysql.pool.query("SELECT course.name, course.course_id FROM course " +
+                    "INNER JOIN student_course ON student_course.c_id = course.course_id " +
+                    "INNER JOIN student ON student.student_id = student_course.s_id " + 
+                    "WHERE student.student_id = ?", [studentId], function (error, result) {
+      if (error) {
+        console.log("Failed to get course links for student : " + studentId);
+        failure(error);
+      } else {
+        console.log("Get courses link for user : " + JSON.stringify(result));
+        success(result);
+      }
+  });
+};
+
+/*********** coursesOverview page to display Course overview that user takes**********/
+
+//return html page for course overview
+
+
+  
+
+/*********** lecture page to display all lectures for selected course that user takes**********/
+
+//return html page of lecture for selected course
+
+//example : enter http://localhost:3000/lectures?course_id=1 to get the lecture overview for student id 1.
+
+
+app.get('/lectures', function (req, res) {
+
+  //debug 
+  // req.session.user_id = 1; 
+
+  var context = {};
+  getAllLecturesForCourse(req.query.course_id, req.session.user_id, 
+    function(lectureList) {
+      context.lectureList = lectureList;
+      res.render('lecture', context);
+    },
+    function(error) {
+      context.errorMessage = JSON.stringify(error);
+      res.render('500', context);
+    }
+  );
+  
+});
+
+//get Lectures API
+app.get('/getLectures', function(req, res, next){
+
+  //debug 
+  // req.session.user_id = 1; 
+
+  getAllLecturesForCourse(req.query.course_id, req.session.user_id,  
+    function(lectureList) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(lectureList));
+    },
+    function(error) {
+      res.send(JSON.stringify(error));
+    }
+  );
+});
+
+
+var getAllLecturesForCourse = function(courseId, studentId, success, failure) {
+  if (!courseId || !studentId) {
+    var errorMessage = "course Id or student Id is invalid";
+    console.log(errorMessage);
+    failure(errorMessage);
+    return;
+  }
+
+  mysql.pool.query("SELECT lecture.title, lecture.body FROM lecture " + 
+                  "INNER JOIN course ON lecture.course_id = course.course_id " + 
+                  "INNER JOIN student_course ON course.course_id = student_course.c_id " +
+                  "WHERE course.course_id = ? AND student_course.s_id = ?", [courseId, studentId], function (error, result) {
+    if (error) {
+      console.log("Failed to get lecture of course " + courseId, "for student ID " + studentId);
+      failure(error);
+    } else {
+      console.log("Get lecture for user : " + JSON.stringify(result));
+      success(result);
+    }
+  });
+};
+
+
+
+/*********** assignments page to display all assignments for selected course that user take **********/
+
+//return html page of assignments for selected course
+//example : enter http://localhost:3000/assignments?course_id=1 to get the assignment for user(student_id=1).
+
+app.get('/assignments', function (req, res) {
+
+  //debug 
+  // req.session.user_id = 1; 
+
+  var context = {};
+  getAllAssignmentsForCourse(req.query.course_id, req.session.user_id, 
+    function(assignmentList) {
+      context.assignmentList = assignmentList;
+      res.render('assignment', context);
+    },
+    function(error) {
+      context.errorMessage = JSON.stringify(error);
+      res.render('500', context);
+    }
+  );
+});
+
+//get assignment API
+app.get('/getAssignments', function(req, res, next){
+
+  //debug 
+  // req.session.user_id = 1; 
+
+  getAllAssignmentsForCourse(req.query.course_id, req.session.user_id,  
+  function(assignmentList) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(assignmentList));
+  },
+  function(error) {
+    res.send(JSON.stringify(error));
+  }
+);
+});
+
+
+var getAllAssignmentsForCourse = function(courseId, studentId, success, failure) {
+  if (!courseId || !studentId) {
+    var errorMessage = "courseId or studentId is invalid";
+    console.log(errorMessage);
+    failure(errorMessage);
+    return;
+  }
+
+  mysql.pool.query("SELECT assignment.title, assignment.questions FROM assignment " + 
+                  "INNER JOIN course ON assignment.course_id = course.course_id " + 
+                  "INNER JOIN student_course ON course.course_id = student_course.c_id " +
+                  "WHERE course.course_id = ? AND student_course.s_id = ?", [courseId, studentId], function (error, result) {
+    if (error) {
+      console.log("Failed to get assignments of course " + courseId, "for student " + studentId);
+      failure(error);
+    } else {
+      console.log("Get assignment for user " + JSON.stringify(result));
+      success(result);
+    }
+  });
+};
 
 
 app.use(function(req,res) {
@@ -148,13 +359,11 @@ app.use(function(req,res) {
   res.render('404');
 });
 
-
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
-
 
 app.listen(app.get('port'), function() {
   console.log('Express started on port:' + app.get('port') + '; press Ctrl-C to terminate.');
