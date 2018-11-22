5e7860e4e4ca05cc7f1c866bde7ccf7de285d154
Tingting Lin <lintin@oregonstate.edu>
2018-11-21 18:26:51 -0800

    change the get profile function and add profile read API

--- server.js
+++ server.js
@@ -105,20 +105,113 @@ app.post('/admin/createuser', checkAdmin, function(req, res, next) {
 })
 
 // Display profile page
-app.get('/profile', function(req, res, next) {
+// app.get('/profile', function(req, res, next) {
+//   var context = {};
+//   mysql.pool.query(
+//     'SELECT ' + req.session.user_type + '_id, first_name, last_name, DOB, identification, user_id FROM ' + req.session.user_type + ' WHERE user_id = ' + req.session.user_id,
+//     function (err, results, fields) {
+//       if(error){
+//         res.write(JSON.stringify(error));
+//         res.end();
+//       } else {
+//       	context.identification = results[0].identification;
+//       	res.render('profile', context);
+//       }
+//   });
+// });
+
+
+/*********** profile page s**********/
+
+//return html page of profile
+
+//example : enter http://localhost:3000/profile to get the lecture overview for student user_id 1 or instructor user_id 4
+
+app.get('/profile', function (req, res) {
+
+  //debug for student ID
+  // req.session.user_id = 1; 
+  // req.session.user_type = "student";
+
+  //or debug for instructor ID
+  // req.session.user_id = 4; 
+  // req.session.user_type = "instructor";
+
   var context = {};
-  mysql.pool.query(
-    'SELECT ' + req.session.user_type + '_id, first_name, last_name, DOB, identification, user_id FROM ' + req.session.user_type + ' WHERE user_id = ' + req.session.user_id,
-    function (err, results, fields) {
-      if(error){
-        res.write(JSON.stringify(error));
-        res.end();
-      } else {
-      	context.identification = results[0].identification;
-      	res.render('profile', context);
-      }
-  });
+  getProfileForUser(req.session.user_type, req.session.user_id, 
+    function(profileResult) {
+      context.profileResult = profileResult;
+      res.render('profile', context);
+    },
+    function(error) {
+      context.errorMessage = JSON.stringify(error);
+      res.render('500', context);
+    }
+  );
+  
 });
+
+//get Profile API
+app.get('/profile', function(req, res, next){
+
+   //debug  choice 1
+  // req.session.user_id = 1; 
+  // req.session.user_type = "student";
+
+  //debug  choice 2
+  // req.session.user_id = 4; 
+  // req.session.user_type = "instructor"; 
+
+  getProfileForUser(req.session.user_type, req.session.user_id,  
+    function(profileResult) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(profileResult));
+    },
+    function(error) {
+      res.send(JSON.stringify(error));
+    }
+  );
+});
+
+
+var getProfileForUser = function(userType, userId, success, failure) {
+  if (!userType || !userId) {
+    var errorMessage = "user type or user ID is invalid";
+    console.log(errorMessage);
+    failure(errorMessage);
+    return;
+  }
+
+  if(userType == "student") {
+
+    mysql.pool.query("SELECT user.user_type, user.user_id, student.first_name, student.last_name, student.DOB, student.identification FROM user " + 
+      "INNER JOIN student ON student.user_id = user.user_id " + 
+      "WHERE user.user_id = ?", [userId], function (error, result) {
+      if (error) {
+        console.log("Failed to get profile for user ID " + userId + JSON.stringify(error));
+        failure(error);
+      } else {
+        console.log("Get profile for user : " + JSON.stringify(result));
+        success(result);
+      }
+      });
+    }
+
+  else if (userType == "instructor") {
+
+    mysql.pool.query("SELECT user.user_type, user.user_id, instructor.first_name, instructor.last_name, instructor.DOB, instructor.identification FROM user " + 
+      "INNER JOIN instructor ON instructor.user_id = user.user_id " +
+      "WHERE user.user_id = ?", [userId], function (error, result) {
+      if (error) {
+        console.log("Failed to get profile for user ID " + userId + JSON.stringify(error));
+        failure(error);
+      } else {
+        console.log("Get profile for user : " + JSON.stringify(result));
+        success(result);
+      }
+    });
+  }
+};
 
 
 app.use(function(req,res) {
